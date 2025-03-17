a89d85008f3f2ca87bf5fc8061a05b60649c383e
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-03-17 11:42:30 -0300

    refactor: update in main.go add server config

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/gmalheiro/playground/internal/application"
+)
+
+func main() {
+	cfg := &application.ConfigServerChi{
+		ServerAddress:  ":8080",
+		LoaderFilePath: "/Users/gsmalheiro/studies/go/playground/docs/db/vehicles_100.json",
+	}
+	app := application.NewServerChi(cfg)
+	if err := app.Run(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
